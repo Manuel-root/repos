2220068a3e2a1299ca4ae531ee445a1e71c4385f
Manuel-root <58568533+Manuel-root@users.noreply.github.com>
2019-12-06 23:46:15 -0300

    Add files via upload

---
+++ i3ins.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#################################################################
+apt install -y acpi acpitool acpi-support acpid intel-microcode fancontrol intel-gpu-tools intel-microcode i965-va-driver mesa-utils mesa-utils-extra xserver-xorg-video-intel xinit laptop-detect
+Xorg --configure
+apt install i3
+xdg-user-dirs-update
+touch .bash_profile
+echo "if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
+  startx
+fi" >> .bash_profile
+cp /etc/X11/xinit/xinitrc /home/manuel/.xinitrc
+echo "exec i3" > ~/.xinitrc
+chown -R manuel:manuel /home/manuel/
+
+
+###### Install main apps, drivers and dependencies
+apt-get install -y libnm-gtk-common
+apt-get install -y pavucontrol
+apt-get install -y alsa-utils
+apt-get install -y python-gconf
+apt-get install -y lxappearance
+apt-get install -y policykit-desktop-privileges
+apt-get install -y policykit-1-gnome
+apt-get install -y thunar
+apt-get install -y gvfs-backends
+apt-get install -y gnome-system-tools
+apt-get install -y network-manager
+apt-get install -y network-manager-gnome
+apt-get install -y wireless-tools
+apt-get install -y overlay-scrollbar overlay-scrollbar-gtk2
+apt-get install -y gnome-bluetooth
+apt-get install -y libbluetooth3 libgnome-bluetooth13
+apt-get install -y pulseaudio-module-bluetooth
+apt-get install -y pulseaudio-module-x11
+apt-get install -y gstreamer1.0-pulseaudio
+apt-get install -y pactl xbacklight
+apt-get install -y feh gnome-icon-theme-full
+apt-get install -y rofi
+apt-get install -y gnome-terminal
+apt-get install -y i3 i3-wm i3blocks i3lock i3status
+apt-get install -y python3-aptdaemon.pkcompat
+apt-get install -y fonts-noto-core
+apt-get install -y xfonts-base xfonts-encodings
+apt-get install -y xfonts-scalable xfonts-utils
+apt-get install -y libxfont1 libfont-afm-perl
+apt-get install -y libfontconfig1 libfontembed1
+apt-get install -y libfontenc1 gnome-font-viewer
+apt-get install -y fontconfig fontconfig-config
+apt-get install -y libxcursor1 xcursor-themes
+apt-get install -y gnome-power-manager
