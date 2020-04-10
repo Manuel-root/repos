f0cef66e042f9bc2e3efb06a6138bbc7dd46477a
Manuel-root <58568533+Manuel-root@users.noreply.github.com>
2020-04-10 13:56:59 -0300

    Update i3ins.sh

--- i3ins.sh
+++ i3ins.sh
@@ -12,7 +12,7 @@ apt install -y ufw
 
 #habilitar contrib non-free
 
-apt install -y acpi acpitool acpi-support acpid intel-microcode xorg xserver-xorg-video-intel i965-va-driver mesa-utils mesa-utils-extra dbus-x11 libatk-adaptor libgail-common firmware-atheros udisks2 pulseaudio rfkill        
+apt install -y acpi acpitool acpi-support acpid intel-microcode xorg xserver-xorg-video-intel i965-va-driver mesa-utils mesa-utils-extra dbus-x11 libatk-adaptor libgail-common firmware-atheros udisks2 network-manager rfkill pulseaudio     
 #Xorg --configure
 
 apt install i3-wm suckless-tools i3blocks xwallpaper
@@ -30,7 +30,6 @@ echo "exec i3" > ~/.xinitrc
 
 chown -R manuel:manuel /home/manuel/
 
-apt install -y network-manager
 apt install -y lfm cmus htop wget curl
 apt install -y sxiv
 apt install -y pavucontrol
@@ -38,4 +37,5 @@ apt install -y policykit-1-gnome gufw
 apt install -y lxappearance papirus-icon-theme
 apt install -y rar unrar zip unzip unace unace-nonfree bzip2 lzop p7zip-full p7zip-rar gzip lzip atril
 apt install -y gtk2-engines gtk2-engines-aurora gtk2-engines-murrine gtk2-engines-sugar gnome-themes-standard gnome-themes-extra papirus-icon-theme
+apt install -y firefox-esr
 apt install -y blueman
